4ed781b9564cea8e01a680dae737b3485727fcd0
hansori01 <19sorihan88@gmail.com>
2021-02-03 22:37:16 +0000

    created the countLetters. pre-test with assertEqual function

--- countLetters.js
+++ countLetters.js
@@ -5,3 +5,29 @@ const assertEqual = (actual, expected) => {
     console.log(`💀 Assertion Failed 💀: ${actual} !== ${expected}`);
   }
 };
+
+
+// take in a str (sentence) and return a count of each of the letters within it.
+
+
+const countLetters = str => {
+  const results = {};
+  // loop through each char of string
+  for (let char of str) {
+    // omits any character that are ' ' space
+    if (char !== ' ') {
+      if (results[char]) {
+        // if the results[char] exists, + 1.
+        results[char] += 1;
+      } else {
+        // if this does not exist - create the key(char) and set it to 1.
+        results[char] = 1;
+      }
+    }
+  }
+  // return results object.
+  return results;
+};
+
+
+console.log(countLetters('hello my name is sori'));
