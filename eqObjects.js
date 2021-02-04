e365cfd10c0365304a0045a47459a8572753736f
hansori01 <19sorihan88@gmail.com>
2021-02-04 05:34:05 +0000

    eslint

--- eqObjects.js
+++ eqObjects.js
@@ -41,8 +41,8 @@ const eqObjects = (object1, object2) => {
 };
 
 
-const cd = { c: "1", d: ["2", 3] };
-const dc = { d: ["2", 3], c: "1" };
+const cd = { d: "1", c: ["2", 3] };
+const dc = { c: ["2", 3], d: "1" };
 assertEqual(eqObjects(cd, dc), true);
 
 const cd2 = { c: "1", d: ["2", 3, 4] };
